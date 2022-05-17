8199bbfa3f19a4cccb9720a8329c5f3dddd73f52
ibrahim fikry <ibrahim.fikry2050@gmail.com>
2022-05-17 08:51:05 +0200

    get data for update

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-var ProductName = document.getElementById('ProductName')
-var ProductCategory = document.getElementById('ProductCategory')
-var ProductPrice = document.getElementById('ProductPrice')
-var ProductDescription = document.getElementById('ProductDescription')
-    // my database
+// my database
 var products = []
+    //   get data
+function getdatatag() {
+    var data = {
+        ProductName: document.getElementById('ProductName'),
+        ProductCategory: document.getElementById('ProductCategory'),
+        ProductPrice: document.getElementById('ProductPrice'),
+        ProductDescription: document.getElementById('ProductDescription')
+    }
+    return data
+}
+
+function getdatainfo() {
+    var profuctinfo = {
+        nameinfo: getdatatag().ProductName.value,
+        Categoryinfo: getdatatag().ProductCategory.value,
+        Priceinfo: getdatatag().ProductPrice.value,
+        Descriptioninfo: getdatatag().ProductDescription.value,
+    }
+    return profuctinfo;
+}
+
+
+// ------------------------------------------clear function 
+function clear() {
+    ProductName.value = ""
+    ProductCategory.value = ""
+    ProductPrice.value = ""
+    ProductDescription.value = ""
+}
 
 // ------------------------------------------  create function   (1)
 function createmodal() {
@@ -16,26 +41,12 @@ function createmodal() {
     document.getElementById('UpdateLabel').style.display = "none"
 }
 
+
+
 function create() {
-
-    var data = {
-        nameinfo: ProductName.value,
-        categoryinfo: ProductCategory.value,
-        priceinfo: ProductPrice.value,
-        descriptioninfo: ProductDescription.value
-    }
-    products.push(data)
-
-    console.log(products)
+    products.push(getdatainfo())
     clear()
     display()
-}
-// ------------------------------------------clear function 
-function clear() {
-    ProductName.value = ""
-    ProductCategory.value = ""
-    ProductPrice.value = ""
-    ProductDescription.value = ""
 }
 
 // ------------------------------------ display function   retrive  (2)
@@ -51,10 +62,10 @@ function display() {
         <tr>
         <th scope="row " hidden>${index}</th>
         <td>${products[index].nameinfo}</td>
-        <td>${products[index].categoryinfo}</td>
-        <td>${products[index].priceinfo}</td>
-        <td>${products[index].descriptioninfo}</td>
-        <td><button class="btn btn-outline-success w-100 " data-bs-toggle="modal" data-bs-target="#staticBackdrop"onclick='updateproduct()'> <i class="fa fa-solid fa-edit "></i></button></td>
+        <td>${products[index].Categoryinfo}</td>
+        <td>${products[index].Priceinfo}</td>
+        <td>${products[index].Descriptioninfo}</td>
+        <td><button class="btn btn-outline-success w-100 " data-bs-toggle="modal" data-bs-target="#staticBackdrop"onclick='getdatabyid(${index})'> <i class="fa fa-solid fa-edit "></i></button></td>
         <td><button class="btn btn-outline-danger w-100 " onclick='deleteproduct(${index})'> <i class="fa fa-solid fa-trash-can "></i></button></td>
         </tr>
         `
@@ -108,11 +119,12 @@ function searchproduct() {
 
             tr += `
         <tr>
-        <th scope="row ">${index}</th>
+        <th scope="row " hidden>${index}</th>
         <td>${products[index].nameinfo}</td>
         <td>${products[index].categoryinfo}</td>
         <td>${products[index].priceinfo}</td>
         <td>${products[index].descriptioninfo}</td>
+        <td><button class="btn btn-outline-success w-100 " data-bs-toggle="modal" data-bs-target="#staticBackdrop"onclick='updateproduct(${index})'> <i class="fa fa-solid fa-edit "></i></button></td>
         <td><button class="btn btn-outline-danger w-100 " onclick='deleteproduct(${index})'> <i class="fa fa-solid fa-trash-can "></i></button></td>
         </tr>
         `
@@ -123,8 +135,8 @@ function searchproduct() {
 }
 
 // --------------   updateproduct
-
-function updateproduct() {
+// get data by id 
+function getdatabyid(id) {
     // btn
     document.getElementById('btnadd').style.display = "none"
     document.getElementById('btnupdate').style.display = ""
@@ -133,5 +145,22 @@ function updateproduct() {
     AddLabel
     document.getElementById('AddLabel').style.display = "none"
     document.getElementById('UpdateLabel').style.display = ""
+
+
+
+    getdatatag().ProductName.value = products[id].nameinfo
+    getdatatag().ProductCategory.value = products[id].Categoryinfo
+    getdatatag().ProductPrice.value = products[id].Priceinfo
+    getdatatag().ProductDescription.value = products[id].Descriptioninfo
+
+
+
+}
+
+function updateproduct(id) {
+
+
+
+
 
 }
